bb9a7413a140df5f41260ece8763519cbb733df8
harin yoo <u.halynn@gmail.com>
2020-01-10 17:43:38 +0900

    고양이

---
+++ 단계별로 풀어보기/Step 01/10171.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Sydney Harin Yoo on 2019-07-20.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\\    /\\" << endl;
+    cout << " )  ( ')" << endl;
+    cout << "(  /  )" << endl;
+    cout << " \\(__)|" << endl;
+    return 0;
+}
