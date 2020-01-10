f310933e1468a2f93770bd0e2380ceb644d8015a
harin yoo <u.halynn@gmail.com>
2020-01-10 17:44:59 +0900

    A-B

---
+++ 단계별로 풀어보기/Step 01/1001.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Sydney Harin Yoo on 2019-07-20.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    cout << A-B;
+    return 0;
+}
