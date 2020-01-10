89c115d7956d372293818fc54ccd16a5c90f4125
harin yoo <u.halynn@gmail.com>
2020-01-10 17:44:18 +0900

    개

---
+++ 단계별로 풀어보기/Step 01/10172.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Sydney Harin Yoo on 2019-07-20.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\" << endl;
+    cout << "|\"^\"`    |" << endl;
+    cout << "||_/=\\\\__|" << endl;
+    return 0;
+}
+
