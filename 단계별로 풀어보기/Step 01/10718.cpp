e97f06b7a11d49bfdbf57beaaedc8e3ff6cae42c
harin yoo <u.halynn@gmail.com>
2020-01-10 17:43:17 +0900

    We love kriii

---
+++ 단계별로 풀어보기/Step 01/10718.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Sydney Harin Yoo on 2019-07-20.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "강한친구 대한육군" << endl;
+    cout << "강한친구 대한육군" << endl;
+    return 0;
+}
