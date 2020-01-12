e0f2c28bae3749f92a5c2aa9fee541f0ff4d5c97
harin yoo <u.halynn@gmail.com>
2020-01-12 19:34:17 +0900

    문자열 반복

--- 단계별로 풀어보기/Step 07/2675.cpp
+++ 단계별로 풀어보기/Step 07/2675.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
-    int test;
-    cin >> test;
-
-    char result[1000][160] = {"\0"};
-    for(int i=0; i<test; i++) {
-        int repeat;
-        cin >> repeat;
+    int t;
+    cin >> t;
+    
+    for(int i=0; i<t; i++) {
+        int r;
+        cin >> r;
         string str;
         cin >> str;
-        for(int j=0; j<str.size(); j++) {
-            for(int k=0; k<repeat; k++) {
-                result[i][j*repeat+k] = str[j];
+        for(int j=0; j<str.length(); j++) {
+            for(int k=0; k<r; k++){
+                cout << str[j];
             }
-        }
-    }
-
-    for(int l=0; l<test; l++){
-        for(int m=0; m<160; m++){
-            if(result[l][m] != NULL) cout << result[l][m];
         }
         cout << endl;
     }
