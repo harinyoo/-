76675b60060e067707e30953c677a83c0327ef58
harin yoo <u.halynn@gmail.com>
2020-02-15 18:34:09 +0900

    단어의 개수

--- 단계별로 풀어보기/Step 07/1152.cpp
+++ 단계별로 풀어보기/Step 07/1152.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
-#include <stdio.h>
-#include <string.h>
 using namespace std;
 
-#define MAX_SIZE 1000000
 int main() {
-    char str[MAX_SIZE];
-    fgets(str,MAX_SIZE,stdin);
+    string str;
+    getline(cin, str);
 
+    int i;
     int cnt = 0;
-    for(int i=0; i<strlen(str)-1; i++){
-        if(str[i]==' ') cnt++;
+    for(i=0; i<str.length(); i++) {
+        if(str[i] == ' ') cnt++;
     }
 
-    if(str[0]==' ' && str[strlen(str)-2]!=' ') cnt -= 1;
-    else if(str[0]!=' ' && str[strlen(str)-2]==' ') cnt -= 1;
-    else if(str[0]==' ' && str[strlen(str)-2]==' ') cnt -= 2;
+    if(str[0] == ' ') cnt--;
+    if(str[str.length()-1] == ' ') cnt--;
 
     cout << cnt+1;
     return 0;
