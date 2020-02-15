858beac35a94ef9414cf2f168212e3a766b128b9
harin yoo <u.halynn@gmail.com>
2020-02-15 18:15:14 +0900

    단어 공부

--- 단계별로 풀어보기/Step 07/1157.cpp
+++ 단계별로 풀어보기/Step 07/1157.cpp
@@ -1,40 +1,39 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
     string str;
     cin >> str;
-    for(int i=0; i<str.size(); i++) {
-        if(str[i]>=97 && str[i]<=122) str[i] -= 32;
+
+    for(int i=0; i<str.length(); i++) {
+        if(str[i]>='a') str[i] -= 32;
     }
 
     char alphabet[26];
-    for(int j=0; j<26; j++) {
-        alphabet[j] = 'A'+j;
-    }
+    for(int j=0; j<26; j++) alphabet[j] = 'A'+j;
 
-    int alpha_num[26] = {0};
-    for(int k=0; k<str.size(); k++) {
-        for(int l=0; l<26; l++){
-            if(str[k]==alphabet[l]) alpha_num[l]++;
+    int cnt[26]={0};
+    for(int k=0; k<str.length(); k++) {
+        for(int l=0; l<26; l++) {
+            if(str[k]==alphabet[l]) cnt[l]++;
         }
     }
 
-    int max = alpha_num[0];
+    int max = cnt[0];
     int idx = 0;
     for(int m=0; m<26; m++) {
-        if(max < alpha_num[m]) {
-            max = alpha_num[m];
+        if(max<cnt[m]) {
+            max = cnt[m];
             idx = m;
         }
     }
 
-    int cnt = 0;
+    int tmp=0;
     for(int n=0; n<26; n++) {
-        if(max == alpha_num[n]) cnt++;
+        if(max==cnt[n]) tmp++;
     }
-    if(cnt == 1) cout << alphabet[idx];
+
+    if(tmp==1) cout << alphabet[idx];
     else cout << "?";
     return 0;
 }
