8c450a2f5cefda45ea654b0042d8127fb2a8d252
harin yoo <u.halynn@gmail.com>
2020-02-18 17:19:10 +0900

    다이얼

--- 단계별로 풀어보기/Step 07/5622.cpp
+++ 단계별로 풀어보기/Step 07/5622.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     string str;
     cin >> str;
 
-    int sec = 0;
-    for(int i=0; i<str.size(); i++) {
-        if(str[i]>='A' && str[i]<='O') sec += (str[i]+1)/3-19;
-        else if(str[i]>='P' && str[i]<='S') sec += 8;
-        else if(str[i]>='T' && str[i]<='V') sec += 9;
-        else if(str[i]>='W' && str[i]<='Z') sec += 10;
-    }
+    vector<string> dial = {{"-1"}, {"-1"}, {"-1"}, {"ABC"}, {"DEF"}, {"GHI"},
+                           {"JKL"}, {"MNO"}, {"PQRS"}, {"TUV"}, {"WXYZ"}};
 
+    int sec = 0;
+    for(int i=0; str[i]; i++) {
+        for(int j=0; j<dial.size(); j++) {
+            if(dial[j].find(str[i]) != string::npos) sec += j;
+        }
+    }
     cout << sec;
     return 0;
 }
