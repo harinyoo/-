6986c05eb69dd0f7261447d8a4e39b1d3a4b2564
harin yoo <u.halynn@gmail.com>
2020-02-18 15:49:30 +0900

    상수

--- 단계별로 풀어보기/Step 07/2908.cpp
+++ 단계별로 풀어보기/Step 07/2908.cpp
@@ -2,22 +2,20 @@
 using namespace std;
 
 int main() {
-    int A, B;
-    cin >> A >> B;
+    string x, y;
+    cin >> x >> y;
 
-    int tmp1, tmp2, tmp3;
-    tmp1 = A/100;
-    tmp2 = (A%100)/10;
-    tmp3 = (A%100)%10;
-    A = tmp3*100 + tmp2*10 + tmp1;
-
-    tmp1 = B/100;
-    tmp2 = (B%100)/10;
-    tmp3 = (B%100)%10;
-    B = tmp3*100 + tmp2*10 + tmp1;
-
-    if(A>B) cout << A;
-    else cout << B;
-
+    string bigger;
+    for(int i=2; i>=0; i--) {
+        if(x[i]>y[i]) {
+            bigger = x;
+            break;
+        }
+        else if(x[i]<y[i]) {
+            bigger = y;
+            break;
+        }
+    }
+    cout << bigger[2] << bigger[1] << bigger[0];
     return 0;
 }
