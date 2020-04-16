7752049b2971e825bfa304c9546302f98a741bea
harin yoo <u.halynn@gmail.com>
2020-04-16 19:35:45 +0900

    한수

--- 단계별로 풀어보기/Step 06/1065.cpp
+++ 단계별로 풀어보기/Step 06/1065.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int han(int n) {
-    int cnt = 0;
-    int diff_1 = 0;
-    int diff_2 = 0;
-    if(n>=100 && n<=1000) {
-        for(int i=100; i<=n; i++) {
-            diff_1 = i/100 - (i%100)/10;
-            diff_2 = (i%100)/10 - (i%100)%10;
-                
-            if(diff_1 == diff_2) cnt++;
-        }
-        cnt += 99;
+bool han(int n) {
+    if(n<100) return true;
+    else if(n>=100) {
+        int x = n/100-(n/10)%10;
+        int y = (n/10)%10-n%10;
+        if(x==y) return true;
+        else return false;
     }
-    else if(n<100) cnt += n;
-    
-    return cnt;
 }
 
 int main() {
     int n;
     cin >> n;
-    cout << han(n);
+
+    int cnt = 0;
+    int i;
+    for(i=1; i<=n; i++) {
+        if(han(i)) cnt++;
+    }
+    cout << cnt;
     return 0;
 }
