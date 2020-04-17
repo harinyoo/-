8aabe4cd5b331dfe9a5e825563a7572b28344eb6
harin yoo <u.halynn@gmail.com>
2020-04-18 01:36:01 +0900

    숫자의 합

--- 단계별로 풀어보기/Step 07/11720.cpp
+++ 단계별로 풀어보기/Step 07/11720.cpp
@@ -2,23 +2,16 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-    int *arr = new int [n];
-    
+    int N;
+    cin >> N;
     string str;
     cin >> str;
-    
-    for(int i=0; i<str.length(); i++) {
-        arr[i] = str[i]-48;
-    }
-    
+
     int sum = 0;
-    for(int j=0; j<n; j++) {
-        sum += arr[j];
+    int i;
+    for(i=0; i<N; i++) {
+        sum += str.at(i)-'0';
     }
-    
     cout << sum;
-    delete[] arr;
     return 0;
 }
