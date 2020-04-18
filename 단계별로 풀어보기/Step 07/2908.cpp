7d8c5f833f39713b9f728963753a0430ecaea67c
harin yoo <u.halynn@gmail.com>
2020-04-18 14:35:59 +0900

    상수

--- 단계별로 풀어보기/Step 07/2908.cpp
+++ 단계별로 풀어보기/Step 07/2908.cpp
@@ -2,13 +2,22 @@
 using namespace std;
 
 int main() {
-    int a, b;
-    cin >> a >> b;
+    string s1, s2;
+    cin >> s1 >> s2;
 
-    int a_ = a%10*100 + (a/10%10)*10 + a/100;
-    int b_ = b%10*100 + (b/10%10)*10 + b/100;
+    string bigger;
+    int i;
+    for(i=2; i>=0; i--) {
+        if(s1[i] > s2[i]) {
+            bigger = s1;
+            break;
+        }
+        else if(s1[i] < s2[i]) {
+            bigger = s2;
+            break;
+        }
+    }
 
-    if(a_ > b_) cout << a_;
-    else cout << b_;
+    for(i=2; i>=0; i--) cout << bigger[i];
     return 0;
 }
