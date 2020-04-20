7b1d8ab488c99015269cc234f2e9c5c83acade68
harin yoo <u.halynn@gmail.com>
2020-04-20 20:56:04 +0900

    벌집

--- 단계별로 풀어보기/Step 08/2292.cpp
+++ 단계별로 풀어보기/Step 08/2292.cpp
@@ -2,18 +2,13 @@
 using namespace std;
 
 int main() {
-    int num;
-    cin >> num;
-
-    int cnt;
-    if(num==1) cnt = 1;
+    int N;
+    cin >> N;
+    if(N==1) cout << 1;
     else {
-        int x = (num-2)/6;
-        int idx = 2;
-        while(x<(idx-1)*(idx-2)/2 || idx*(idx-1)/2-1<x) { idx++; }
-        cnt = idx;
+        int i=1;
+        while(N<3*(i-1)*i+2 || 3*i*(i+1)+2<=N) { i++; }
+        cout << i+1;
     }
-
-    cout << cnt;
     return 0;
 }
