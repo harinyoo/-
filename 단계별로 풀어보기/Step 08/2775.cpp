55c59a903131a44bb8ef7eba4a394a632633fd9b
harin yoo <u.halynn@gmail.com>
2020-04-21 16:16:51 +0900

    부녀회장이 될테야

--- 단계별로 풀어보기/Step 08/2775.cpp
+++ 단계별로 풀어보기/Step 08/2775.cpp
@@ -2,26 +2,17 @@
 using namespace std;
 
 int people(int k, int n) {
-    int apt[15][14] = {0};
-    int i;
-    for(i=0; i<n; i++) {
-        apt[0][i] = i+1;
-    }
-
-    for(i=1; i<=k; i++) {
-        apt[i][0] = 1;
-        for(int j=1; j<n; j++) {
-            apt[i][j] = apt[i][j-1] + apt[i-1][j];
-        }
-    }
-
-    return apt[k][n-1];
+    if(k==0) return n;
+    if(n==1) return 1;
+    return people(k, n-1) + people(k-1,n);
 }
 
 int main() {
-    int t, k, n;
-    cin >> t;
-    for(int i=0; i<t; i++) {
+    int T, k, n;
+    cin >> T;
+
+    int i;
+    for(i=0; i<T; i++) {
         cin >> k >> n;
         cout << people(k, n) << endl;
     }
