73a1c44f02b639162b25f89674dac29535f655cd
harin yoo <u.halynn@gmail.com>
2020-04-25 23:45:09 +0900

    택시 기하학

---
+++ 단계별로 풀어보기/Step 10/3053.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int R;
+    cin >> R;
+    cout << fixed;
+    cout.precision(6);
+    cout << M_PI*R*R << endl << 2*R*R;
+    return 0;
+}
