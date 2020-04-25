216a61b87f2de8b2fd6c733e3b8645712f19c9d2
harin yoo <u.halynn@gmail.com>
2020-04-25 19:14:18 +0900

    직각삼각형

---
+++ 단계별로 풀어보기/Step 10/4153.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int length[3];
+    int i, j, tmp;
+    while(cin >> length[0] >> length[1] >> length[2]){
+        if(length[0]==0 || length[1]==0 || length[2]==0) break;
+        else{
+            sort(length, length+3);
+            if(pow(length[2],2) == pow(length[0],2)+pow(length[1],2)) cout << "right" << endl;
+            else cout << "wrong" << endl;
+        }
+    }
+    return 0;
+}
