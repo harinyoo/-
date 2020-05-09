9fa5019bc3f0c3f81cd28205f768f0099b597832
harin yoo <u.halynn@gmail.com>
2020-05-09 16:49:57 +0900

    N과 M (4)

---
+++ 단계별로 풀어보기/Step 15/15652.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+#define SIZE 9
+int N, M;
+int arr[SIZE];
+int picked[SIZE];
+
+void tree( int cnt) {
+    int i;
+    if(cnt == M) {
+        for(i=0; i<M; i++) {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+        return;
+    }
+
+    for(i=1; i<=N; i++) {
+        if(picked[i]<=M) {
+            if(arr[cnt-1]<=i) {
+                picked[i]++;
+                arr[cnt] = i;
+                tree(cnt+1);
+                picked[i]--;
+            }
+        }
+    }
+}
+
+int main() {
+    int i;
+
+    cin >> N >> M;
+    for(i=0; i<M; i++) {
+        arr[i] = 0;
+        picked[i] = 0;
+    }
+
+    tree(0);
+    return 0;
+}
